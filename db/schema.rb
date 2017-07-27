893347fb56840f7cf91f639028d4b1af97268ecc
girwarkishor <girwar.kishor@gmail.com>
2017-07-27 13:54:51 +0530

    Add Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725123850) do
+ActiveRecord::Schema.define(version: 20170726090930) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "link_id"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "title"
